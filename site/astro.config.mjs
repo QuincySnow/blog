77ec155708612499254c09c0b18c0fb1249f00ed
QuincySnow <146918156+QuincySnow@users.noreply.github.com>
2026-03-14 12:06:44 +0800

    Add base path configuration for site deployment

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https:/quincysnow.github.io',
+	base: '/site/',
 	integrations: [mdx(), sitemap()],
 });
